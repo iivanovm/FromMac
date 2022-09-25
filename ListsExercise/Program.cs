21ed064bf6cc2775c774d948d926b16921d9e44a
wizard <vacho@mail.bg>
2022-09-25 09:13:22 +0300

    AddExercise
    Add Project for List Exercise , and start first task

---
+++ ListsExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Exer
+    {
+        public static void Train()
+        {
+            List<int> wagon = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxPass = int.Parse(Console.ReadLine());
+            string commnad;
+            while ((commnad = Console.ReadLine()) != "end")
+            {
+                string[] ninp = commnad.Split();
+                if (ninp[0] == "Add")
+                {
+                    wagon.Add(int.Parse(ninp[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagon.Count; i++)
+                    {
+                        if (wagon[i] + int.Parse(ninp[0]) <= maxPass)
+                        {
+                            wagon[i] += int.Parse(ninp[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
